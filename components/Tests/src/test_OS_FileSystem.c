361f40873ce95a5385c7300a1024b98cc32aa716
Benedikt Driessen <benedikt.driessen@hensoldt-cyber.de>
2020-10-19 18:41:13 +0200

    [REN] Adapt to renamed instantiation modes

--- components/Tests/src/test_OS_FileSystem.c
+++ components/Tests/src/test_OS_FileSystem.c
@@ -321,7 +321,7 @@ test_OS_FileSystem_mount_fail(void)
 {
     static OS_Crypto_Config_t cfgCrypto =
     {
-        .mode = OS_Crypto_MODE_LIBRARY_ONLY,
+        .mode = OS_Crypto_MODE_LIBRARY,
         .entropy = IF_OS_ENTROPY_ASSIGN(
             entropy_rpc,
             entropy_port),
